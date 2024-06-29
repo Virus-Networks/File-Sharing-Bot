be21272cbe85027c127b3f98ddc72989d826bf9a
Virus-Networks <168676321+Virus-Networks@users.noreply.github.com>
2024-06-29 08:26:21 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>M͜͡ Ʀ⩔ɨƦ⩏ន₀₂</a>/n○ Language : <code>Python3</code>/n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>/n○ Channel : @Virus_Botz/n○ Support Group : <a href='https://t.me/VIRUS_BOTZ_SUPPORT_GROUP'>Click here</a></b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>M͜͡ Ʀ⩔ɨƦ⩏ន₀₂</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @Virus_Botz\n○ Support Group : <a href='https://t.me/VIRUS_BOTZ_SUPPORT_GROUP'>Click here</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
